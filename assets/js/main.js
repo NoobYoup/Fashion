ca0962a2c188432c701f4a9fd9eabfcd3d53a4a0
NoobYoup <134846824+NoobYoup@users.noreply.github.com>
2024-05-16 09:59:33 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// const nav=document.querySelector(".element-translate")
+// const menu=document.querySelector(".modal-menu");
+// const menulist=document.querySelector(".menu-mobile")
+// const overlay=document.querySelector(".overlay-menu");
+// menulist?.addEventListener("click",(e)=>{
+//     menu.classList.toggle("active")
+//     overlay.classList.toggle("active")
+// })
+// console.log(nav)
+// nav?.addEventListener("click",(e)=>{
+//     menu.classList.toggle("active")
+//     overlay.classList.toggle("active");
+// })
